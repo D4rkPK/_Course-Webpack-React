33cecabfea91c04b7691a9244f1d3474341eb314
bjpc <brandonpineda4@gmail.com>
2022-04-28 12:57:36 -0600

    :bug: Fix components

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-const path = requere('path');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
     entry: './src/index.js',
@@ -9,27 +11,35 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx']
     },
-    module:{
-        rulser: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader,'
-                }
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
             }
+          },
+          {
+            test: /\.html$/,
+            use: [
+              { loader: 'html-loader' }
+            ]
+          }
         ]
-    },
+      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ],
     devServer: {
-        static: 
-        {
-          directory: path.join(__dirname, "dist"),
-          watch: true,
+        static: {
+        directory: path.join(__dirname, 'dist'),
         },
-        watchFiles: path.join(__dirname, "./**"), //observa los cambios en todos nuestros archivos y actualiza el navegador
         compress: true,
-        historyApiFallback: true,
-        port: 3006,
-        open: true, //Hace que se abra en el navegador
+        port: 8080,
+        open: true,
     }
 }
